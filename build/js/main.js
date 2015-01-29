7840a3f41aed3fc0ba929e0dca16da1af255f775
James C. Palmer <palmer.james@gmail.com>
2015-01-29 14:27:27 -0500

    minor js tweaks

--- build/js/main.js
+++ build/js/main.js
@@ -6,30 +6,54 @@ $(function () {
 
   particlesJS('particles', {
     particles: {
-      color: '#555',
+      color: '#fff',
       shape: 'circle',
       opacity: 1,
       size: 2.5,
       size_random: true,
-      nb: $(window).width() <= 600 ? 100 : 200,
+      nb: $(window).width() <= 600 ? 50 : 200,
       line_linked: {
         enable_auto: true,
         distance: 100,
-        color: '#454545',
+        color: '#fff',
         opacity: 1,
         width: 1,
         condensed_mode: {
           enable: true,
-          rotateX: 6500,
-          rotateY: 6500
+          rotateX: 65000,
+          rotateY: 65000
         }
       },
       anim: {
         enable: true,
-        speed: 1
+          speed: 1
+      },
+      array: []
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 100
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: 1
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
       }
     },
-    retina_detect: true
+    retina_detect: false,
+    fn: {
+      vendors:{
+        interactivity: {}
+      }
+    }
   });
 
 });
